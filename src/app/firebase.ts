1d93bb67c7a0166f83dbec9aff9cf052c94d6680
selima <selimaomrani95@gmail.com>
2019-04-24 11:44:15 +0100

    first commit

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyAjdmUsiSzLapkwD0MgoeNRI16ukwHtTAU",
+  authDomain: "projetmodule-c38a7.firebaseapp.com",
+  databaseURL: "https://projetmodule-c38a7.firebaseio.com",
+  projectId: "projetmodule-c38a7",
+  storageBucket: "projetmodule-c38a7.appspot.com",
+  messagingSenderId: "518989594644"
+};
+
+export default config;
